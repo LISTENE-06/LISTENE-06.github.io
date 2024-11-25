f8e2ba3bfdc0bd9b8c309c405081349de20291c0
LISTENER <3426779909@qq.com>
2024-11-25 23:06:46 +0800

    Site updated: 2024-11-25 23:06:45

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732370185940";
+const VERSION = "1732547191198";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
